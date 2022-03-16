264a470e942398f55f28a1914cde150d42a184ca
anthony-ohannessian <anthony.ohannessian@laplateforme.io>
2022-03-16 17:26:08 +0100

    job00 done

--- jour02/job05/script.js
+++ jour02/job05/script.js
@@ -1,20 +1,20 @@
-var pos = document.getElementById('footer');
+// var pos = document.getElementById('footer');
 
-var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
-window.addEventListener('scroll', function(){
-    var progress = (document.documentElement.scrollTop / height) * 100;
+// window.addEventListener('scroll', function(){
+//     var progress = (document.documentElement.scrollTop / height) * 100;
 
-    console.log(progress);
-    pos.style.width = progress + "%";
+//     console.log(progress);
+//     pos.style.width = progress + "%";
 
-    if (progress <= 35 ){
-        pos.style.background = "blue";
-    }
-    else if (progress > 35  && progress <= 70 ) {
-        pos.style.background = "pink";
-    }
-    else if (progress > 70  && progress <= 100 ) {
-        pos.style.background = "red";
-    }
-})
+//     if (progress <= 35 ){
+//         pos.style.background = "blue";
+//     }
+//     else if (progress > 35  && progress <= 70 ) {
+//         pos.style.background = "pink";
+//     }
+//     else if (progress > 70  && progress <= 100 ) {
+//         pos.style.background = "red";
+//     }
+// })
