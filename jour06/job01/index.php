10580dec52b761d4f009782d6bea8c7b41c3af70
anthony-ohannessian <anthony.ohannessian@laplateforme.io>
2022-03-21 14:35:49 +0100

    jour06 job01 done

--- jour06/job01/index.php
+++ jour06/job01/index.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 1920px)" href="style2.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 1680px)" href="style3.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (max-width: 1280px)" href="style4.css"/>
+    
     <title>Document</title>
 </head>
 <body>
